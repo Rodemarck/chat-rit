7d387e37a777ddbf9599d7018bba2bfdb3896b9a
rodemarck <rodemarck.meloj@ufrpe.br>
2019-11-20 10:51:16 -0300

    n quer pegar...

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -6,14 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                    .permitAll()
-            .and()
-                .anonymous()
-                    .disable()
-                        .exceptionHandling()
-                            .authenticationEntryPoint(new Http401AuthenticationEntryPoint("headerValue"))
+        http.authorizeRequests().antMatchers("/").permitAll()
             .and()
                 .csrf()
                     .disable();
