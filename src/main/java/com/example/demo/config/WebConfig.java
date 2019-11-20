c801220dd521497115694271a135854706fba299
rodemarck <rodemarck.meloj@ufrpe.br>
2019-11-20 10:30:03 -0300

    'oi'

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
