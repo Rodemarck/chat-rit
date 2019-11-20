94cbfcd586a08de75fb65b9ec9f0c24fa8fb4416
rodemarck <rodemarck.meloj@ufrpe.br>
2019-11-20 10:09:01 -0300

    a

--- src/main/java/com/example/demo/controller/IndecController.java
+++ src/main/java/com/example/demo/controller/IndecController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class IndecController {
-
+    @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping("/")
-    public String index(){
-        return "index";
+    public ModelAndView index(){
+        return new ModelAndView( "index");
     }
 }
