4be780713a44032a5dd959e8c9ff00bd0a4f1ca8
rodemarck <rodemarck.meloj@ufrpe.br>
2019-11-23 00:37:24 -0300

    'anonimos

--- src/main/java/com/example/demo/controller/IndecController.java
+++ src/main/java/com/example/demo/controller/IndecController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 @CrossOrigin(origins = "http://domain2.com", maxAge = 3600)
 @RestController
 public class IndecController {
-    @RequestMapping("/")
-    public ModelAndView index(){
+    @RequestMapping("/{token}")
+    public ModelAndView index(@PathVariable(value="token") String token){
+        System.out.println("meu token é [" + token + "]");
         return new ModelAndView("index");
     }
 }
